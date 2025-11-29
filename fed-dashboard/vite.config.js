f43758501a18c9fc67da09ac1f89d583cd0502c9
gopi12158388 <2400030720@kluniversity.in>
2025-11-29 19:19:33 +0530

    first commit

--- fed-dashboard/vite.config.js
+++ fed-dashboard/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 5173
-  }
-})
+  base: "./", // important for relative assets on deployment
+});
